062110f87bbd66b02a1c9257cb8860cf95c91160
Erin Wu <erinwu25@gmail.com>
2018-07-30 10:50:47 -0700

    working version

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import jinja2
 import os
 
 from google.appengine.ext import ndb
+from google.appengine.api import users
 
-class Post(ndb.Model):
-    message = ndb.StringProperty()
-    username = ndb.StringProperty()
-    created_time = ndb.DateTimeProperty(auto_now_add=True)
+class Student(ndb.Model):
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    birthday = ndb.DateProperty()
+
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -18,21 +20,32 @@ env = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        posts = Post.query().fetch()
-        post_query = Post.query()
-        post_query = post_query.order(Post.created_time)
+        login_url = ''
+        logout_url = ''
+        #current user will be a user object or NONE
+        current_user = users.get_current_user()
+        #if no one is logged in, show a login prompt
+        if not current_user:
+            login_url = users.create_login_url('/')
+        else:
+            logout_url = users.create_logout_url('/')
+        # posts = Post.query().fetch()
+        # post_query = Post.query()
+        # post_query = post_query.order(Post.created_time)
         templateVars = {
-            'posts' : posts,
+            'current_user' : current_user,
+            'login_url' : login_url,
+            'logout_url' : logout_url,
         }
-        template = env.get_template('/templates/cssiwall.html')
+        template = env.get_template('/templates/home.html')
         self.response.write(template.render(templateVars))
 
-    def post(self):
-        message = self.request.get('message')
-        username = self.request.get('username')
-        post = Post(message=message, username=username)
-        post.put()
-        self.redirect('/')
+    # def post(self):
+    #     message = self.request.get('message')
+    #     username = self.request.get('username')
+    #     post = Post(message=message, username=username)
+    #     post.put()
+    #     self.redirect('/')
 
 
 app = webapp2.WSGIApplication([
