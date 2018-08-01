ef072b7fad82a3c04cf43883a848409629610ac7
Erin Wu <erinwu25@gmail.com>
2018-08-01 11:02:56 -0700

    ughhhhh

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,17 @@
-let wikipedia = document.querySelector('#wikipedia');
-let dictionary = document.querySelector('#dictionary');
-let iframe = document.querySelector('iframe');
+// let wikipedia = document.querySelector('#wikipedia');
+// let dictionary = document.querySelector('#dictionary');
+// let iframe = document.querySelector('iframe');
 let button = document.querySelector('button');
+let p = document.querySelector('.lq1');
+
+alert("Hello! I am an alert box!!");
+
 
 button.addEventListener('click', e => {
-  // let img = document.createElement('img');
-  // img.src = 'https://thumbs-prod.si-cdn.com/azaY6UJQr6w7GMO5NScOuYhUbIM=/800x600/filters:no_upscale()/https://public-media.smithsonianmag.com/filer/ef/ff/efff9ae5-1832-489f-bb1f-f1a00944a8aa/hedgehog.jpg'
-  // img.height = '100'
-  // document.body.appendChild(img);
-  let p = document.querySelector('p');
   if(p.style.display == 'none'){
     p.style.display = 'block'; }
   else{
-    p.style.display = 'none';
+     p.style.display = 'none';
 
   }
 });
@@ -30,12 +29,12 @@ button.addEventListener('click', e => {
 //   iframe.src = 'https://dictionary.com/'
 // });
 
-window.addEventListener('keydown', e => {
-  if (e.key == 'ArrowLeft'){
-    window.location.href = '/';
-  }
-  else if (e.key == 'ArrowRight'){
-    window.location.href = '/second';
-
-  }
-});
+// window.addEventListener('keydown', e => {
+//   if (e.key == 'ArrowLeft'){
+//     window.location.href = '/';
+//   }
+//   else if (e.key == 'ArrowRight'){
+//     window.location.href = '/second';
+//
+//   }
+// });
